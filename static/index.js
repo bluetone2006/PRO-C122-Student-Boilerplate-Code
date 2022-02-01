bf92b00ebbf48d5054fcfcee4311f2e288b0501a
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-01 19:13:09 +0530

    Update index.js

--- static/index.js
+++ static/index.js
@@ -101,10 +101,9 @@ function askBot() {
         }
 
     })
-    $('#bot_input_text').keypress(function(e){
-        //If Enter key(key code is 13) pressed
-        if(e.which == 13){         
-            $('#send_button').click(); //Trigger Send Button Click Event
+   
+      //Send message if Enter key(key code is 13) pressed message 
+        
         }
     });
 }
